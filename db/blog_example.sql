7d8ed82136c095727964d88a5bfe2700c5eca1a8
Alan Russell <ajrussellaudio@gmail.com>
2018-04-27 22:04:50 +0100

    add comments table

--- db/blog_example.sql
+++ db/blog_example.sql
@@ -1,3 +1,4 @@
+DROP TABLE comments;
 DROP TABLE posts;
 
 CREATE TABLE posts (
@@ -5,4 +6,11 @@ CREATE TABLE posts (
   title VARCHAR(255),
   body TEXT,
   posted_on DATE
+);
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  body TEXT,
+  posted_on DATE,
+  post_id INT REFERENCES posts(id)
 );
