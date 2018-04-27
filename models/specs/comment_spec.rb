6f34f6730444698f7e9b5eeaa10878d3e670174e
Alan Russell <ajrussellaudio@gmail.com>
2018-04-27 22:13:39 +0100

    add comment tests

---
+++ models/specs/comment_spec.rb
@@ -0,0 +1,34 @@
+require("minitest/autorun")
+require("minitest/rg")
+require("date")
+
+require_relative("../comment")
+
+class CommentTest < MiniTest::Test
+
+  def setup
+    @sample_text = "This is a test comment."
+    @comment = Comment.new({
+      "body" => @sample_text,
+      "posted_on" => Date.today.to_s,
+      "post_id" => "1"
+    })
+  end
+
+  def test_has_body
+    assert_equal(@sample_text, @comment.body)
+  end
+
+  def test_has_date
+    assert_equal(Date.today, @comment.posted_on)
+  end
+
+  def test_has_post_id
+    assert_equal(1, @comment.post_id)
+  end
+
+  def test_has_id
+    assert_equal(0, @comment.id)
+  end
+
+end
