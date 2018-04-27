d563dce0066a4c654c0a8b666c2dfa884b3e4323
Alan Russell <ajrussellaudio@gmail.com>
2018-04-27 21:32:50 +0100

    add seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+require_relative("../models/post")
+
+post1 = Post.new({
+  "title" => "First Post",
+  "body" => "Lorem ipsum dolor sit amet, eos ei everti volutpat convenire. No viris noluisse argumentum sea. Ne sit causae apeirian accusamus, sed case ullum quando ei. Civibus accommodare at vis.",
+  "date" => Date.today.to_s
+})
+
+post2 = Post.new({
+  "title" => "Second Post",
+  "body" => "Ridens audire saperet an mei. Te dicam perfecto vis. Per prompta impedit imperdiet et, ei sea idque quidam impedit, nisl zril praesent te mel. Verterem vituperatoribus ad qui. Mei partiendo consulatu an.",
+  "date" => Date.today.to_s
+})
